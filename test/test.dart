8e6d85f0a3421532f457b0ffe44c2bc398d2b84e
Hadrien Lejard <hadrien.lejard@gmail.com>
2015-09-10 21:26:30 +0200

    test

--- test/test.dart
+++ test/test.dart
@@ -1,4 +1,5 @@
 // Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 library symcrypt.test;
@@ -7,15 +8,17 @@ import 'package:symcrypt/core.dart';
 import 'package:test/test.dart';
 
 void main() {
-/*  group('A group of tests', () {
-    Awesome awesome;
+  String data;
+  String key;
+  String encryptData;
+  String decryptData;
 
-    setUp(() {
-      awesome = new Awesome();
-    });
+  data = "Toto";
+  key = Symcrypt.createHash("Key");
+  encryptData = Symcrypt.encrypt(data, key);
+  decryptData = Symcrypt.decrypt(encryptData, key);
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });*/
+  test('encrypt/decrypt', () {
+    expect(decryptData, data);
+  });
 }
